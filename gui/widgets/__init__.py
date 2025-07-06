229c313739374002d9573583f4c0b60a4fd22f79
slipfaith <128168725+Slipfaith@users.noreply.github.com>
2025-07-07 01:08:04 +0300

    Add SDXLIFF split/merge UI

---
+++ gui/widgets/__init__.py
@@ -0,0 +1,12 @@
+from .drop_area import SmartDropArea
+from .sdxliff_drop_area import SdxliffDropArea
+from .file_list import FileListWidget
+from .progress_widget import ProgressWidget
+
+__all__ = [
+    "SmartDropArea",
+    "SdxliffDropArea",
+    "FileListWidget",
+    "ProgressWidget",
+]
+
