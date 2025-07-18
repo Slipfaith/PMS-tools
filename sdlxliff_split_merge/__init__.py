d15bf65489a932cd0aeb6f6907bcce9c7d839d4b
slipfaith <128168725+Slipfaith@users.noreply.github.com>
2025-07-18 10:57:16 +0300

    Add balanced word split and targeted merge

--- sdlxliff_split_merge/__init__.py
+++ sdlxliff_split_merge/__init__.py
@@ -8,6 +8,7 @@ from .splitter import StructuralSplitter
 from .merger import StructuralMerger
 from .validator import SdlxliffValidator
 from .io_utils import make_split_filenames, save_bytes_list, read_bytes_list, sort_split_filenames
+from .target_merge import merge_with_original
 from .xml_utils import TransUnitParser, XmlStructure
 
 # Импорты для обратной совместимости с main.py
@@ -26,6 +27,7 @@ __all__ = [
     "save_bytes_list",
     "read_bytes_list",
     "sort_split_filenames",
+    "merge_with_original",
     # Старые имена для совместимости
     "Splitter",
     "Merger"
